612379665236ba5a45d633fda22ce3b2629af67b
Thomas <nyumat18@gmail.com>
2022-11-25 04:08:45 -0800

    Backend Entry Point Cleaned up

--- backend/main.go
+++ backend/main.go
@@ -1,33 +1,25 @@
 package main
 
 import (
-	"log"
-	"os"
 	"backend/configs"
+	"backend/routes"
+	"fmt"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	"github.com/joho/godotenv"
 )
-
-type Todo struct {
-	ID    int    `json:"id"`
-	Title string `json:"title"`
-	Done  bool   `json:"done"`
-	Body  string `json:"body"`
-}
 
 func main() {
 	// Create new Fiber instance
 	app := fiber.New(fiber.Config{
 		CaseSensitive: true,
 		StrictRouting: true,
-		ServerHeader:  "Fiber",
-		AppName:       "Fiber",
+		ServerHeader:  "FiberM8",
+		AppName:       "~ GolangGoBackend ~",
 	})
-
-	configs.MongoConnect()
 
 	// Middleware 
 	app.Use(logger.New())
@@ -40,64 +32,14 @@ func main() {
 	))
 
 	// Routes
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"msg": "Hello Nyumat!",
-		})
-	})
+	routes.UserRoutes(app)
 
-	Todos := [] Todo {
-		{ID: 1, Title: "Wash Nyumat's Clothes", Done: false, Body: "Body 1"},
-		{ID: 2, Title: "Clean Nyumat's Codebase", Done: false, Body: "Body 2"},
-		{ID: 3, Title: "Find Nyumat better food", Done: false, Body: "Body 3"},
-	}
+	// MongoDB Connection
+	configs.MongoConnect()
 
-	app.Get("/todos", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"success": true,
-			"data":    Todos,
-		})
-	})
+	fmt.Printf("%sSuccess: Hi Nyumat, Welcome back to MongoDB!%s ", configs.ColorGreen, configs.ColorReset)
 
-	app.Get("/todos/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
-		return c.SendString("Todo " + id)
-	})
-
-	app.Post("/todos", func(c *fiber.Ctx) error {
-		type Request struct {
-			Title string `json:"title"`
-			Body  string `json:"body"`
-		}
-
-		var body Request
-
-		if err := c.BodyParser(&body); err != nil {
-			return err
-		}
-
-		Todos = append(Todos, Todo{
-			ID:    len(Todos) + 1,
-			Title: body.Title,
-			Done:  false,
-			Body:  body.Body,
-		})
-
-		return c.JSON(fiber.Map{
-			"success": true,
-			"data":    Todos,
-		})
-	})
-
-
-	// Start server with port from .env
-	port := func() string {
-		err := godotenv.Load(".env")
-		if err != nil {
-			log.Fatal("Error loading .env file")
-		}
-		return os.Getenv("PORT")
-	}
-
-	log.Fatal(app.Listen(":" + port()))
+	// Listen to port from .env
+	port := configs.PORT()
+	log.Fatal(app.Listen(":" + port))
 }
