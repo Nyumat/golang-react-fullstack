d3103d61701c321340d474fbab74dd11048d810e
Thomas <nyumat18@gmail.com>
2022-11-25 03:56:41 -0800

    Added Color Constants For Console Printing

---
+++ backend/configs/colors.go
@@ -0,0 +1,27 @@
+package configs
+
+/* 
+ * colors.go is a constants file 
+ * that contains all the colors
+ * used for writing to the console.
+*/
+const (
+	// ColorReset resets the color
+	ColorReset = "\033[0m"
+	// ColorRed sets the color to red
+	ColorRed = "\033[31m"
+	// ColorGreen sets the color to green
+	ColorGreen = "\033[32m"
+	// ColorYellow sets the color to yellow
+	ColorYellow = "\033[33m"
+	// ColorBlue sets the color to blue
+	ColorBlue = "\033[34m"
+	// ColorPurple sets the color to purple
+	ColorPurple = "\033[35m"
+	// ColorCyan sets the color to cyan
+	ColorCyan = "\033[36m"
+	// ColorWhite sets the color to white
+	ColorWhite = "\033[37m"
+	// ColorBlack sets the color to black
+	ColorBlack = "\033[30m"
+) 
